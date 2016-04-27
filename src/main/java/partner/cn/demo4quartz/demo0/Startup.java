5b286920150ce08ec196fd025064e9ebf1f4b387
Yonsin Joe <qiao.yongxin@navercorp.com>
2016-04-27 11:33:37 +0800

    ff

---
+++ src/main/java/partner/cn/demo4quartz/demo0/Startup.java
@@ -0,0 +1,38 @@
+package partner.cn.demo4quartz.demo0;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+import static org.quartz.TriggerKey.triggerKey;
+
+import java.util.Date;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerFactory;
+import org.quartz.Trigger;
+
+/**
+ * @author qiao.yongxin
+ * @date 2015年9月7日
+ */
+@Slf4j
+public class Startup {
+
+    public static void main(String[] args) throws Exception {
+        SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
+        Scheduler scheduler = schedFact.getScheduler();
+        scheduler.start();
+        JobDetail job = newJob(HelloJob.class).withIdentity("myJob").build();
+        Trigger trigger =
+                newTrigger().withIdentity(triggerKey("myTrigger", "myTriggerGroup")).withSchedule(simpleSchedule())
+                        .startAt(new Date()).build();
+
+        scheduler.scheduleJob(job, trigger);
+        log.info("Returns the next time at which the Trigger is scheduled to fire:{}", trigger.getNextFireTime());
+        log.info("Get the time at which the Trigger should quit repeating :{}", trigger.getEndTime());
+        log.info("Returns the last time at which the Trigger will fire:{}", trigger.getFinalFireTime());
+    }
+}
